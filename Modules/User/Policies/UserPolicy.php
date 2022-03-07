9a8f7159819cff8da545fb48a70d70b66f3c43f3
idanieldrew <danielgia35@gmail.com>
2022-03-07 15:35:05 +0330

    Add super user for authorization

--- Modules/User/Policies/UserPolicy.php
+++ Modules/User/Policies/UserPolicy.php
@@ -9,6 +9,13 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    public function before($user,$ability)
+    {
+        if ($user->isSuper()){
+            return true;
+        }
+    }
+
     /**
      * Determine whether the user can view any models.
      *
