be8a9eab2f4dafe8af580df56b7bcbd4946c0d1f
idanieldrew <danielgia35@gmail.com>
2022-05-17 18:10:06 +0430

    Fix configure for postgres in docker

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
+\Illuminate\Support\Facades\Route::get('/',function (){
+    dd(
+        env('DB_CONNECTION'),
+        env('DB_HOST'),
+        env('DB_PORT'),
+        env('DB_DATABASE')
+    );
+});
+
 
