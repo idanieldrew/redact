0e1bc1737f39f120fd46c2f67e6bbddc9663c05f
idanieldrew <danielgia35@gmail.com>
2022-05-26 15:46:33 +0430

    Test for search query

--- Modules/Post/tests/Feature/ReadPostTest.php
+++ Modules/Post/tests/Feature/ReadPostTest.php
@@ -4,12 +4,14 @@ namespace Module\Post\tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Module\Category\Models\Category;
 use Module\Post\Models\Post;
+use function PHPSTORM_META\type;
 use Tests\TestCase;
 
 class ReadPostTest extends TestCase
 {
-    use RefreshDatabase,WithFaker;
+    use RefreshDatabase, WithFaker;
 
     /** @test */
     public function read_posts()
@@ -21,5 +23,20 @@ class ReadPostTest extends TestCase
 
         $this->get(route('post.index'))
             ->assertOk();
+    }
+
+    /** @test */
+    public function search_posts()
+    {
+        $user = $this->CreateUser();
+        $posts = Post::factory()->count(20)->raw();
+        $post = Post::factory()->count(1)->raw(['title' => 'test two']);
+
+        $user->posts()->createMany($posts);
+        $user->posts()->createMany($post);
+
+        $this->getJson(route('post.search') . '? keyword=test')
+            ->assertOk()
+            ->assertJsonFragment(['title' => 'test two']);
     }
 }
