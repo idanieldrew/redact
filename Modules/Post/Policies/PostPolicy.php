8e0e14cce4276f9021977cbb2af396c4cfc27fb4
idanieldrew <danielgia35@gmail.com>
2022-07-05 15:15:42 +0430

    Replace === with == in post policy

--- Modules/Post/Policies/PostPolicy.php
+++ Modules/Post/Policies/PostPolicy.php
@@ -2,6 +2,7 @@
 
 namespace Module\Post\Policies;
 
+use Illuminate\Auth\Access\Response;
 use Module\Post\Models\Post;
 use Module\User\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -11,61 +12,27 @@ class PostPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      *
-     * @param  \Module\User\Models\User  $user
-     * @param  \Module\Post\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Post $post
+     * @return bool
      */
-    public function update(User $user, Post $post)
+    public function update(User $user, Post $post): bool
     {
-        return $user->id == $post->user_id;
+        return $user->id === $post->user_id;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \Module\User\Models\User  $user
-     * @param  \Module\Post\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Post $post
+     * @return bool
      */
-    public function delete(User $user, Post $post)
+    public function delete(User $user, Post $post): bool
     {
-        return $user->id == $post->user_id;
+        return $user->id === $post->user_id;
     }
 
     /**
@@ -73,7 +40,7 @@ class PostPolicy
      *
      * @param  \App\Models\User  $user
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @return Response|bool
      */
     public function restore(User $user, Post $post)
     {
@@ -85,7 +52,7 @@ class PostPolicy
      *
      * @param  \App\Models\User  $user
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @return Response|bool
      */
     public function forceDelete(User $user, Post $post)
     {
