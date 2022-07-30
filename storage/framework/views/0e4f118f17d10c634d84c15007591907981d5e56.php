cfd7392eba1974162054c47005470f011b09d6c2
idanieldrew <danielgia35@gmail.com>
2022-07-30 15:18:25 +0430

    Tests for storing post

--- storage/framework/views/0e4f118f17d10c634d84c15007591907981d5e56.php
+++
@@ -1,6 +0,0 @@
-dear admin
-<a href="<?php echo e(env('APP_URL') . "/api/posts/${slug}"); ?>">
-<?php echo e($slug); ?>
-
-</a>
-<?php /**PATH /home/danieldrew/pro/laravel/weblog/Modules/Post/Providers/../Resources/views/Post/Mail/PostPublishedPermission.blade.php ENDPATH**/ ?>
