69d416e54cfe42a3dfdb631e9178818560fbe4cf
idanieldrew <danielgia35@gmail.com>
2022-09-05 16:19:46 +0430

    Add tests for role module

---
+++ Modules/Role/tests/Unit/AssignRoleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Module\Role\tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Module\Role\Models\Permission;
+use Module\Role\Models\Role;
+use Module\User\Models\User;
+use Tests\TestCase;
+
+class AssignRoleTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function assign_role_to_users()
+    {
+        $role = Role::create(['name' => 'writer']);
+        $user = User::factory()->create();
+
+        $user->assignRole($role->name);
+
+        $this->assertTrue($role->users->contains($user));
+        $this->assertEquals(1, $role->users->count());
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $role->users);
+    }
+
+    /** @test */
+    public function relation_roles_and_permissions()
+    {
+        $role = Role::create(['name' => 'writer']);
+        $permission = Permission::create(['name' => 'create-post']);
+        $role->givePermissionTo($permission);
+
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $role->permissions);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $permission->role_permissions);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Module\Category\Database\Seeders\CategorySeeder;
-use Module\Category\Models\Category;
-use Module\Image\Models\Image;
-use Module\Post\Models\Post;
 use Module\Role\Database\Seeders\RoleTableSeeder;
-use Module\Tag\Models\Tag;
 use Module\User\Models\User;
 
 class DatabaseSeeder extends Seeder
