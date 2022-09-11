b4a1506772813cce64384e506ba95b5ba3888cf5
idanieldrew <danielgia35@gmail.com>
2022-09-11 14:58:29 +0430

    Reply comment

--- Modules/Comment/Database/Migrations/2022_09_06_151713_create_comments_table.php
+++ Modules/Comment/Database/Migrations/2022_09_06_151713_create_comments_table.php
@@ -17,6 +17,7 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->string('body');
             $table->morphs('commentable');
+            $table->unsignedBigInteger('parent_id');
             $table->timestamps();
         });
     }
--- Modules/Comment/Models/Comment.php
+++ Modules/Comment/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace Module\Comment\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Comment extends Model
 {
@@ -10,9 +11,19 @@ class Comment extends Model
 
     /**
      * Get the parent commentable model (post or ...).
+     *
+     * @return MorphTo
      */
-    public function commentable()
+    public function commentable(): \Illuminate\Database\Eloquent\Relations\MorphTo
     {
         return $this->morphTo();
+    }
+
+    /**
+     * For replies
+     */
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
     }
 }
