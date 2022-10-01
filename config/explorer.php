5c22c450d1d918fcb6fb4420d03b94c0f3954a45
idanieldrew <danielgia35@gmail.com>
2022-10-01 15:56:26 +0330

    fix elastic 2

--- config/explorer.php
+++ config/explorer.php
@@ -9,7 +9,7 @@ return [
      * https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/configuration.html
      */
     'connection' => [
-        'host' => 'elasticsearch',
+        'host' => 'medium_elasticsearch',
         'port' => '9200',
         'scheme' => 'http',
     ],
