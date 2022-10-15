b5c9d33c1aa6b25fe7a42864466305aa5f015d87
idanieldrew <danielgia35@gmail.com>
2022-10-15 10:28:06 +0330

    Fix tests

--- storage/framework/views/cbb6281a9218f04df553c6a018ca1b6e80ebc8db.php
+++
@@ -1,6 +0,0 @@
-dear admin
-<a href="<?php echo e(env('APP_URL') . "/api/category/${slug}"); ?>">
-    <?php echo e($slug); ?>
-
-</a>
-<?php /**PATH /var/www/Modules/Category/Providers/../Resources/Views/Category/Mail/CategoryPublishedPermission.blade.php ENDPATH**/ ?>
