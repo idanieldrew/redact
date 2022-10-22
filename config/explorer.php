dab4f8e88a6f74781920525d1a16bd7b44f0da5d
Daniel <danielgia35@gmail.com>
2022-10-22 17:47:34 +0330

    Update explorer.php

--- config/explorer.php
+++ config/explorer.php
@@ -9,7 +9,7 @@ return [
      * https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/configuration.html
      */
     'connection' => [
-        'host' => 'elasticsearch,
+        'host' => 'elasticsearch',
         'port' => '9200',
         'scheme' => 'http',
     ],
