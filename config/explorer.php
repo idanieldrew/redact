38d286647fd7536583f469945e26b397145c4588
idanieldrew <danielgia35@gmail.com>
2022-10-25 15:29:52 +0330

    fix env example ci 2

--- config/explorer.php
+++ config/explorer.php
@@ -9,7 +9,7 @@ return [
      * https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/configuration.html
      */
     'connection' => [
-        'host' => 'mediumElasticsearch',
+        'host' => 'localhost',
         'port' => '9200',
         'scheme' => 'http',
     ],
